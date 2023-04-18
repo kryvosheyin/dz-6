b72616cd72b38010656bf3adbad000b4c531ebb5
kryvosheyin <alex.kryvosheyin@gmail.com>
2023-04-18 10:23:10 +0100

    Refactored according to suggested notes

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,44 @@
 import java.util.*;
+import static java.util.Collections.emptySet;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] inputArray = {"mama","bbbbbbp", "caabb", "abccbas", "xyz", "laslas"};
-        String[] result = (findEvenCharacterStrings(inputArray));
-        char[] chars = uniqueCharacters(result);
-
-        System.out.print("Found unique characters are: ");
-        for(Character c : chars){
-            System.out.print(c+" ");
-        }
+        String[] inputArray = {"mamda", "bbbbbb2", "кукуeкуку", "caabb", "abccbas", "xyz", "laslasr"};
+        System.out.print("Found unique characters are: " + findEvenCharacterStrings(inputArray));
     }
-
-    public static String[] findEvenCharacterStrings(String[] inputArray) {
-        String[] result = new String[2];
+    public static Set findEvenCharacterStrings(String[] inputArray) {
         int resultCount = 0;
+        Set finalSetOfCharacters = new HashSet();            //створюється сет для унікальних симолів
 
         for (String s : inputArray) {
-            if (hasEvenCharacters(s)) {
-                result[resultCount++] = s;
+            Set uniqueCharsForWord = hasEvenCharacters(s);
+            if (!uniqueCharsForWord.isEmpty()) {
+                finalSetOfCharacters.addAll(uniqueCharsForWord);   //всі унікальні символи слова додаються до фінального
+                resultCount++;
                 if (resultCount == 2) {
                     break;
                 }
             }
         }
-
-        if (resultCount < 2) {
-            String[] resizedResult = new String[resultCount];
-            System.arraycopy(result, 0, resizedResult, 0, resultCount);
-            return resizedResult;
-        }
-
-        return result;
+        return finalSetOfCharacters;
     }
-
-    public static boolean hasEvenCharacters(String s) {
+    public static Set hasEvenCharacters(String s) {
         Map<Character, Integer> charFrequency = new HashMap<>();
-
+        Set uniqueCharacters = new HashSet();
         for (char c : s.toCharArray()) {
             charFrequency.put(c, charFrequency.getOrDefault(c, 0) + 1);
+            uniqueCharacters.add(c);                 // додаються унікальні символи для слова.
         }
 
         for (int frequency : charFrequency.values()) {
             if (frequency % 2 != 0) {
-                return false;
+                return emptySet();                       // якщо умова не виконана - тоді ми повертаємо пустий сет
             }
         }
 
-        return true;
+        return uniqueCharacters;
     }
 
-    public static char[] uniqueCharacters(String[] words){
-        Set<Character> uniqueChars = new HashSet<>();
-        for(String s : words){
-            for (char c :s.toCharArray()){
-                uniqueChars.add(c);
-            }
-        }
-        char[] result = new char[uniqueChars.size()];
-        int index = 0;
-
-        for (Character c : uniqueChars){
-            result[index++]=c;
-        }
-        return result;
-    }
 }
